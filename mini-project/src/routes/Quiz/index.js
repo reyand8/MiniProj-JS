4d03411c31a462df351c7c857683072044af79da
Andrey <andreeey.gg@gmail.com>
2023-11-24 17:47:11 +0100

    update quiz and add input

--- mini-project/src/routes/Quiz/index.js
+++ mini-project/src/routes/Quiz/index.js
@@ -3,36 +3,58 @@ import {useState} from "react";
 
 const questions = [
     {
-        title: 'React - это ... ?',
-        variants: ['библиотека', 'фреймворк', 'приложение'],
-        correct: 0,
+        title: 'JavaScript File Has An Extension of:',
+        variants: ['.Java', '.Js', '.javascript', '.xml'],
+        correct: '.Js',
+        type: 'question',
         id: 1,
     },
     {
-        title: 'Компонент - это ... ',
-        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-        correct: 1,
+        title: 'The Tag is used To Give Heading To The Table',
+        variants: ['Head', 'Td', 'Th', 'Caption'],
+        correct: 'Caption',
+        type: 'question',
         id: 2,
     },
     {
-        title: 'Что такое JSX?',
+        title: 'IsNaN() Evaluates And Argument To Determine if Given Value:',
         variants: [
-            'Это простой HTML',
-            'Это функция',
-            'Это тот же HTML, но с возможностью выполнять JS-код',
+            'Is Not a Null',
+            'Is Not a Number',
+            'Is Not a New Object',
+            'None Of The Above',
         ],
-        correct: 2,
+        correct: 'Is Not a Number',
+        type: 'question',
         id: 3,
     },
+    {
+        title: 'Method Prompt() Contain...Number of Parameters',
+        answer: '',
+        correct: '2',
+        type: 'input',
+        id: 4,
+    },
+    {
+        title: 'Prompt() Display a Message And a Data Entry Field',
+        variants: [
+            'True',
+            'False',
+        ],
+        correct: 'True',
+        type: 'question',
+        id: 5,
+    },
+
 ];
 
 function Result({correct}) {
     return (
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt='result-img'/>
-            <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+            <h2>You have {correct} points of {questions.length}</h2>
             <a href="/quiz/">
-                <button>Попробовать снова</button>
+                <button>Try again</button>
             </a>
 
         </div>
@@ -40,31 +62,55 @@ function Result({correct}) {
 }
 
 function Game({step, question, onClickVariant}) {
-    const {title, variants} = question
+    const [input, setInput] = useState('')
+    const {title, variants, type} = question
     const percentage = Math.round((step / questions.length) * 100)
+
+    function onInputChange(e) {
+        setInput(e.target.value)
+    }
+    function onSubmit(e) {
+        e.preventDefault()
+        setInput('')
+        onClickVariant(input)
+    }
+
+    function askQuestions() {
+        if (type === 'question') {
+            question = <ul>
+                            {variants.map((text, index) =>
+                                <li onClick={() => onClickVariant(text)} key={index}>{text}</li>
+                        )}
+                        </ul>
+        }
+        else if (type === 'input') {
+            question =
+                    <form className='input-question' onSubmit={onSubmit}>
+                        <input maxLength="4" type='text' value={input} onChange={onInputChange} key={7771}/>
+                        <button className='btn-step'>Next Step</button>
+                    </form>
+        }
+        return question
+    }
+
     return (
         <>
             <div className="progress">
                 <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
             </div>
             <h1>{title}</h1>
-            <ul>
-                {variants.map((text, index) =>
-                    <li onClick={() => onClickVariant(index)} key={text}>{text}</li>
-                )}
-            </ul>
+            {askQuestions()}
         </>
-    );
+    )
 }
 
 function Quiz() {
     const [step, setStep] = useState(0)
     const [correct, setCorrect] = useState(0)
     const question = questions[step]
-
-    const onClickVariant = (index) => {
+    const onClickVariant = (selected) => {
         setStep(step + 1)
-        if (index === question.correct) {
+        if (selected === question.correct) {
             setCorrect(correct + 1)
         }
     }
