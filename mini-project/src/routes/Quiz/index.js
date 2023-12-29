ea5e6b758a8acd96072c895270abcbb31658b8cd
Andrey <andreeey.gg@gmail.com>
2023-12-29 13:41:03 +0100

    fix href error

--- mini-project/src/routes/Quiz/index.js
+++ mini-project/src/routes/Quiz/index.js
@@ -56,7 +56,7 @@ function Result({correct}) {
         <div className="quiz__result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="result-img"/>
             <h2>You have {correct} points of {questions.length}</h2>
-            <a href="/quiz/">
+            <a href="/quiz">
                 <button>Try again</button>
             </a>
 
